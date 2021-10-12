ec6689d8d3a65825d5b4051adac28aef9013cf89
Luvdipsingh Dadiyala <ldadiyala@gmail.com>
2021-10-13 02:00:39 +0530

    till redux

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -10,5 +10,13 @@ export const getPost = async (req, res) => {
 };
 
 export const createPost = async (req, res) => {
-  res.send("Controllers running");
+  const post = req.body;
+  const newPost = new PostMessage(post);
+
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
 };
