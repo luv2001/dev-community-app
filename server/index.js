2cfd85786b694d969a5cd4a9f4eda0474dd9e5ce
Luvdipsingh Dadiyala <ldadiyala@gmail.com>
2021-10-13 01:07:50 +0530

    routers and controllers set

--- server/index.js
+++ server/index.js
@@ -4,15 +4,15 @@ import bodyParser from "body-parser";
 import cors from "cors";
 const PORT = 5500 || process.env.PORT;
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("HEMLO LEMLO MEMRA");
-});
+app.use("/posts", postRoutes);
 
 const DATABASE_URL =
   "mongodb+srv://dbdevapp:dbdevapp@cluster0.dhsuh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
